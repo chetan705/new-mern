7f6fed88369b11b73346ce44d254816ba7de9d93
Chetan Prakash Sharmae <chetanprakashshama@gmail.com>
2025-04-14 17:28:24 +0530

    all changes committed

---
+++ job-backend/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
+const providerRoutes = require("./routes/provider");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+require('dotenv').config(); 
+const MONGO_URI = process.env.MONGO_URI || `mongodb+srv://chetanprakashshama:demo123@cluster0.dr6kt.mongodb.net/demo`;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+
+app.use("/auth", authRoutes);
+app.use("/admin", adminRoutes);
+app.use("/provider", providerRoutes);
+app.use("/user", userRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
+});
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Database");
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
